cd69625bfc013ea19a256d22e083b7241a3fa098
errata-c <erratachem@gmail.com>
2021-11-17 13:51:25 -0500

    Updated the configuration for proper installation

--- cmake/preconfig.cmake
+++ cmake/preconfig.cmake
@@ -1,3 +1,11 @@
 if(NOT TARGET SQLiteCpp)
 	find_dependency(SQLiteCpp CONFIG)
 endif()
+
+if(NOT TARGET fmt::fmt)
+	find_dependency(fmt CONFIG)
+endif()
+
+if(NOT TARGET xxHash::xxhash)
+	find_dependency(xxHash CONFIG)
+endif()
