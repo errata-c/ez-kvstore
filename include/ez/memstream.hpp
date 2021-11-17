bee4c2417048a63931391d6cc039c9f27d33cb6c
errata-c <erratachem@gmail.com>
2021-11-17 15:34:16 -0500

    Minor changes to membuf impl

--- include/ez/memstream.hpp
+++ include/ez/memstream.hpp
@@ -4,6 +4,11 @@
 namespace ez {
 	class membuf : public std::streambuf {
 	public:
+		using char_type = char;
+		using int_type = std::streambuf::int_type;
+		using pos_type = std::streambuf::pos_type;
+		using off_type = std::streambuf::off_type;
+
 		membuf()
 		{
 			reset();
@@ -17,6 +22,18 @@ namespace ez {
 			reset(first, len);
 		}
 
+		membuf(membuf&& other)
+		{}
+		membuf& operator=(membuf&& other) {
+			membuf tmp{ std::move(other) };
+			this->swap(tmp);
+
+			return *this;
+		}
+
+		membuf(const membuf&) = delete;
+		membuf& operator=(const membuf&) = delete;
+
 		void reset(const char* first, std::size_t len) {
 			reset(first, first + len);
 		}
@@ -26,27 +43,36 @@ namespace ez {
 		void reset() {
 			this->setg(0, 0, 0);
 		}
+
+		void swap(membuf & other) {
+			std::streambuf::swap(other);
+		}
 	protected:
 		pos_type seekoff(off_type off, std::ios_base::seekdir dir, std::ios_base::openmode which = std::ios_base::in) override
 		{
+			pos_type res = pos_type(off_type(-1));
+
 			switch (dir) {
 			case std::ios_base::cur:
-				gbump(off);
+				this->gbump(off);
+				res = this->gptr() - this->eback();
 				break;
 			case std::ios_base::end:
-				setg(eback(), egptr() + off, egptr());
+				this->setg(this->eback(), this->egptr() + off, this->egptr());
+				res = this->gptr() - this->eback();
 				break;
 			case std::ios_base::beg:
-				setg(eback(), eback() + off, egptr());
+				this->setg(this->eback(), this->eback() + off, this->egptr());
+				res = this->gptr() - this->eback();
 				break;
 			}
 
-			return gptr() - eback();
+			return res;
 		}
 
 		pos_type seekpos(pos_type sp, std::ios_base::openmode which) override
 		{
-			return seekoff(sp - pos_type(off_type(0)), std::ios_base::beg, which);
+			return this->seekoff(sp - pos_type(off_type(0)), std::ios_base::beg, which);
 		}
 	};
 
@@ -56,7 +82,6 @@ namespace ez {
 			: std::istream(&mbuf)
 			, mbuf()
 		{}
-
 		imemstream(const char * data, std::size_t size)
 			: std::istream(&mbuf)
 			, mbuf(data, size)
@@ -65,6 +90,9 @@ namespace ez {
 			: std::istream(&mbuf)
 			, mbuf(data, end)
 		{}
+
+		imemstream(imemstream&&) = default;
+		imemstream& operator=(imemstream&&) = default;
 
 		void reset(const char* first, const char* last) {
 			mbuf.reset(first, last);
--- tests/basic/streambuf.cpp
+++ tests/basic/streambuf.cpp
@@ -39,3 +39,21 @@ TEST_CASE("immutable test") {
 		REQUIRE(str[i] == '\0');
 	}
 }
+
+TEST_CASE("swapping membuf") {
+	char arr0[] = "testing";
+	char arr1[] = "something";
+
+	ez::membuf buf0{ arr0, sizeof(arr0) - 1 };
+	ez::membuf buf1{ arr1, sizeof(arr1) - 1 };
+
+	buf0.swap(buf1);
+
+	std::istream in0(&buf0), in1(&buf1);
+	std::string v0, v1;
+	std::getline(in0, v0);
+	std::getline(in1, v1);
+
+	REQUIRE(v0 == "something");
+	REQUIRE(v1 == "testing");
+}
